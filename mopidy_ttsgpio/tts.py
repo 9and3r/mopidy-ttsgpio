7fb1212ab97bca6301d9826258a594f8935bba28
9and3r <9and3r@gmail.com>
2016-03-24 13:12:21 +0100

    Change from Google TTS to Festival

--- mopidy_ttsgpio/tts.py
+++ mopidy_ttsgpio/tts.py
@@ -1,6 +1,5 @@
-import urllib
-
-import gst
+import os
+from threading import Thread
 
 music_level = 30
 
@@ -9,24 +8,10 @@ class TTS():
 
     def __init__(self, frontend, config):
         self.frontend = frontend
-        self.player = gst.element_factory_make("playbin", "tts")
-        output = gst.parse_bin_from_description(config['audio']['output'],
-                                                ghost_unconnected_pads=True)
-        volume = config['ttsgpio']['tts_default_volume']
-        self.player.set_property('volume', volume)
-        self.player.set_property('audio-sink', output)
 
     def speak_text(self, text):
-        self.player.set_state(gst.STATE_NULL)
-        params = {}
-        params['tl'] = 'en'
-        params['q'] = text.encode('ascii', 'ignore')
-        music_stream_uri = 'http://translate.google.com/translate_tts?' \
-                           + urllib.urlencode(params)
-        self.player.set_property('uri', music_stream_uri)
-        self.player.set_state(gst.STATE_PLAYING)
+        t = Thread(target=self.speak_text_thread, args=(text,))
+        t.start()
 
-        bus = self.player.get_bus()
-        bus.enable_sync_message_emission()
-        bus.add_signal_watch()
-        # bus.connect('message::eos', self.end_of_stream)
+    def speak_text_thread(self, text):
+        os.system(' echo "' + text + '" | festival --tts')
