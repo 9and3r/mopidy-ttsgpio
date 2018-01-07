50df4a6c50b41256e09e804e929013980d5c68ec
Herb Peyerl <hpeyerl+gitlab@beer.org>
2018-01-07 22:34:03 +0000

    use 'festival --pipe' to decrease latency

--- mopidy_ttsgpio/tts.py
+++ mopidy_ttsgpio/tts.py
@@ -1,17 +1,20 @@
 import os
 from threading import Thread
+import time
 
 music_level = 30
 
 
 class TTS():
+	def __init__(self):
+		self.t = Thread(target=self.speak_text_thread)
+		self.t.start()
 
-    def __init__(self, frontend, config):
-        self.frontend = frontend
+	def speak_text(self, text):
+		s='(SayText "{0}")\n'.format(text)
+		self.p.stdin.write(s)
 
-    def speak_text(self, text):
-        t = Thread(target=self.speak_text_thread, args=(text,))
-        t.start()
-
-    def speak_text_thread(self, text):
-        os.system(' echo "' + text + '" | festival --tts')
+	def speak_text_thread(self):
+		self.p = subprocess.Popen('/usr/bin/festival --pipe'.split(),stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+		while 1:
+			time.sleep(1)
